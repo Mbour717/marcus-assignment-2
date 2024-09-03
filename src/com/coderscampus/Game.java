8cd20806cefaa388edbb296431da262fa29fd3c4
Marcus Bourdeau <Marcusx717@outlook.com>
2024-09-03 16:56:30 -0400

    v-1

--- src/com/coderscampus/Game.java
+++ src/com/coderscampus/Game.java
@@ -3,34 +3,33 @@ package com.coderscampus;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Players {
+public class Game {
 	Scanner scanner = new Scanner(System.in);
 	Random random = new Random();
 	int input;
 	int randomNumber;
-	int zero;
-	
-	public int result (int maxAttempts) {
+	int incrementNum;
+	int maxAttempts = 5;
+
+	public int getInput() {
 		randomNumber = random.nextInt(100) + 1;
-		
-		while (zero < maxAttempts) {
+
+		while (incrementNum < maxAttempts) {
 			System.out.println("Pick a number between 1 and 100: ");
 			input = scanner.nextInt();
-			
+			incrementNum++;
+
 			if (input < 1 || input > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
-			}else {
-				zero++;
-			}
-			
-			if (input > randomNumber) {
+			} else if (input > randomNumber) {
 				System.out.println("Please pick a lower number \n");
-			}else if (input < randomNumber) {
+			} else if (input < randomNumber) {
 				System.out.println("Please pick a higher number \n");
-			}else {
+			} else {
 				System.out.println("You win!");
 				return input;
 			}
+
 		}
 		System.out.println("You lose! \n " + "\n" + "The number you guess was: " + randomNumber);
 		return input;
---
+++ src/com/coderscampus/GuessingGame.java
@@ -0,0 +1,11 @@
+package com.coderscampus;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		Game playerInput = new Game();
+
+		playerInput.getInput();
+	}
+
+}

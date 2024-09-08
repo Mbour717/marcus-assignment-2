582fe8daea333ed83687555b4129ea03a2818912
Marcus Bourdeau <Marcusx717@outlook.com>
2024-09-08 08:17:44 -0400

    v-2-Final

--- src/com/coderscampus/Game.java
+++ src/com/coderscampus/Game.java
@@ -17,13 +17,20 @@ public class Game {
 		while (incrementNum < maxAttempts) {
 			System.out.println("Pick a number between 1 and 100: ");
 			input = scanner.nextInt();
-			incrementNum++;
 
 			if (input < 1 || input > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
 			} else if (input > randomNumber) {
+				incrementNum++;
+				if (incrementNum == maxAttempts) {
+					break;
+				}
 				System.out.println("Please pick a lower number \n");
 			} else if (input < randomNumber) {
+				incrementNum++;
+				if (incrementNum == maxAttempts) {
+					break;
+				}
 				System.out.println("Please pick a higher number \n");
 			} else {
 				System.out.println("You win!");
